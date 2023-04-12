bc22a47242ca96afa09c1701fa66bbd720fe3970
poorvaditya behre <aditya.behre@gmail.com>
2023-04-12 11:46:57 +0530

    added codes for chat logic

--- index.js
+++ index.js
@@ -13,14 +13,26 @@ io.on("connection", (socket) => {
   console.log("a user connected ", socket.id);
 
   // now we want to listen the event "from_client"
-  socket.on("from_client", () => {
-    console.log("Event coming from Client");
-  });
+  // socket.on("from_client", () => {
+  //   console.log("Event coming from Client");
+  // });
 
-  setInterval(() => {
-    // server is sending event "from_server" to the client
-    socket.emit("from_server");
-  }, 2000);
+  // setInterval(() => {
+  //   // server is sending event "from_server" to the client
+  //   socket.emit("from_server");
+  // }, 2000);
+
+  // listenner -->
+  socket.on("msg_send", (data) => {
+    console.log(data);
+    // emmitter -->
+
+    // io.emit("msg_rcvd", data);  // io.emit() --> is for all the clients connected to the web server
+
+    // socket.emit("msg_rcvd", data); // socket.emit() --> is for the same client which send the event will only listen to it .
+
+    socket.broadcast.emit("msg_rcvd", data); // except the original one(the one who emits) other two clients will receive the event. 
+  });
 });
 
 // instead of using express app server , now use http module server
